312a1ed86d17d6bd5ebb4ed1b4c56257240487bd
KP <q6894kjz8x34@gmail.com>
2025-10-09 09:14:40 +0200

    finished frontend
    bottom text

--- web-frontend/src/app/page.tsx
+++ web-frontend/src/app/page.tsx
@@ -1,10 +1,19 @@
+import { fetchDevices } from "@/api/device";
 import HomePagePromo from "./homePagePromo";
 import { getAuthData } from "@/lib/auth/server";
+import DeviceDashboard from "@/components/device/dashboard/dashboard";
 
 export default async function Home() {
-  const loggedIn = !!(await getAuthData());
-
+  const authData = await getAuthData();
+  const loggedIn = !!authData;
   if (loggedIn) {
-    return <h1>Dashboard</h1>;
+    const devices = await fetchDevices(authData?.token);
+    if (devices instanceof Error) {
+      throw devices;
+    }
+    if (devices.length === 0) {
+      return <h1>You don't have any devices yet!</h1>;
+    }
+    return <DeviceDashboard device={devices[0]} />;
   } else return <HomePagePromo />;
 }
