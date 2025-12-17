925c9d81257fec71bab9bfaabb7f9d5202f6f7da
Klaudia <klaudia.kp21@gmail.com>
2025-12-17 12:02:42 +0100

    wip commit

--- video-ai-service/add_notif.py
+++ video-ai-service/add_notif.py
@@ -1,9 +1,15 @@
 import requests
+import enum
 BACKEND_URL = "http://127.0.0.1:8000/api/v1/notification/debug_add"  
+class NotificationType(enum.Enum):
+    Info = "Info"
+    Warning = "Warning"
+    Critical = "Critical"
 
-def send_notification(message: str):
+
+def send_notification(type: NotificationType, message: str):
     payload = {
-        "type": "Info",
+        "type": type.value,
         "message": message
     }
 
